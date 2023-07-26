58a2bb207b27a84354a9b61c9004f28935b967cf
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-26 23:32:31 +0530

    update.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6">
+      <h1>About Page</h1>
+      <p class="fs-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem iste facere culpa ea hic quis
+        modi sit quo, labore, placeat magnam soluta sint, fuga officia illum quod vitae sunt
+        tempora!
+      </p>
+      <p class="fs-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quibusdam laboriosam
+        expedita inventore quam iure eos quidem, beatae est odio non officia, eveniet eum laudantium
+        eius fuga quisquam! Consequatur, quisquam? Repellat necessitatibus amet consectetur harum,
+        pariatur sapiente reiciendis recusandae excepturi facere nam, voluptatem in unde
+        perspiciatis animi, iure cumque quasi. Eveniet est suscipit quae a enim at aliquam vero et,
+        cumque doloremque ut. Aut optio praesentium aspernatur sed libero harum minima, reiciendis
+        ab est nesciunt laboriosam, debitis ad aperiam consectetur? Necessitatibus aspernatur porro
+        dicta facilis pariatur eligendi omnis magnam! Praesentium optio sequi numquam quasi cum et
+        labore minima, facilis odit accusantium doloribus assumenda, corrupti dicta, commodi qui ea
+        nam in. Sequi incidunt deserunt alias perferendis obcaecati exercitationem necessitatibus
+        unde, nobis ex ea dolore error voluptatibus, blanditiis distinctio adipisci. Reprehenderit
+        natus magnam beatae ea eum maiores magni quisquam molestiae laborum saepe quod aspernatur
+        distinctio quam quaerat consequatur obcaecati voluptatibus laboriosam maxime nemo,
+        voluptates possimus eaque ab harum ex! Eum velit reiciendis accusantium architecto non,
+        incidunt soluta vel esse repellat earum ipsa debitis consequatur et quam voluptatum
+        laboriosam. Laudantium illo commodi laborum doloribus. Tempore, nulla error? Provident unde
+        quidem explicabo, porro illum adipisci veritatis earum eius in tempore, dolorem alias facere
+        et?
+      </p>
+    </div>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,31 @@
 </script>
 
 <template>
-  <main>
-    <h1>This is Home</h1>
-  </main>
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6">
+      <h1>Home Page</h1>
+      <p class="fs-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem iste facere culpa ea hic quis
+        modi sit quo, labore, placeat magnam soluta sint, fuga officia illum quod vitae sunt
+        tempora! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vero voluptatibus unde
+        ipsum ipsa provident earum aliquam repudiandae minima nulla esse cupiditate, fugit ullam
+        consequuntur quam voluptatem incidunt excepturi magnam, nesciunt perferendis, voluptate
+        exercitationem ad. Placeat veniam fuga accusantium sint! Rerum impedit tempore alias quae
+        asperiores? Quasi rem totam dicta, earum, dolores doloribus labore omnis mollitia, ab
+        voluptatem error enim veniam! Blanditiis molestiae similique reiciendis vitae doloremque
+        repudiandae nostrum. Libero minus dolore culpa impedit quaerat quod, recusandae molestias
+        velit, maxime nam odio repellat voluptatem animi fuga ratione quos quisquam, quae incidunt
+        aspernatur doloremque ullam? Iusto fuga enim dignissimos recusandae expedita modi distinctio
+        ea temporibus repellat, aut ipsum velit est accusantium animi vel perspiciatis esse vitae
+        placeat, ipsa magni impedit, beatae delectus! Commodi molestias inventore repellat,
+        cupiditate explicabo tempora nam molestiae veniam illo minima? Veniam, ipsam nisi! Laborum
+        veritatis cum officiis nemo odit fugit? Doloremque illo dolorum sed voluptas laborum et
+        placeat, obcaecati fugit delectus voluptatibus nobis maiores. Exercitationem deleniti fugiat
+        laboriosam itaque debitis, odio atque molestiae. Explicabo inventore saepe pariatur
+        veritatis eveniet ut sequi omnis quasi perspiciatis numquam. Ipsam dolores, aut cupiditate
+        praesentium voluptatibus eveniet repellendus in possimus laboriosam quia. Cum suscipit
+        magnam quidem ea numquam in possimus. Cum, quae.
+      </p>
+    </div>
+  </div>
 </template>
