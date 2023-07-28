e5f2f57d0eea4b364e8e934063eeb19eeb70d9c2
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-28 09:21:24 +0530

    with responseive.

--- src/views/MyValidationDemo2.vue
+++ src/views/MyValidationDemo2.vue
@@ -19,38 +19,46 @@ let saveForm = () => {
 
 
 <template>
-  <h1 class="alert alert-secondary">Basic Validation</h1>
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6">
+      <h1 class="alert alert-secondary">Basic Validation</h1>
+      <form
+        class="needs-validation"
+        :class="{ 'was-validated': isFormSubmited }"
+        novalidate
+        ref="myform"
+      >
+        <div>
+          <input
+            type="text"
+            placeholder="Enter username"
+            class="form-control form-control-lg mt-1"
+            required
+            minlength="3"
+          />
+          <div class="valid-feedback">Looks good!</div>
+          <div class="invalid-feedback">Please choose a username.</div>
+        </div>
 
-  <form
-    class="needs-validation"
-    :class="{ 'was-validated': isFormSubmited }"
-    novalidate
-    ref="myform"
-  >
-    <div>
-      <input
-        type="text"
-        placeholder="Enter username"
-        class="form-control form-control-lg mt-1"
-        required
-        minlength="3"
-      />
-      <div class="valid-feedback">Looks good!</div>
-      <div class="invalid-feedback">Please choose a username.</div>
+        <div>
+          <input
+            type="email"
+            placeholder="Enter email"
+            class="form-control form-control-lg mt-1"
+            required
+            minlength="3"
+          />
+          <div class="valid-feedback">Looks good!</div>
+          <div class="invalid-feedback">Please choose a Email.</div>
+        </div>
+
+        <input
+          type="button"
+          value="Submit"
+          class="btn btn-secondary w-100 mt-2"
+          @click="saveForm"
+        />
+      </form>
     </div>
-
-    <div>
-      <input
-        type="email"
-        placeholder="Enter email"
-        class="form-control form-control-lg mt-1"
-        required
-        minlength="3"
-      />
-      <div class="valid-feedback">Looks good!</div>
-      <div class="invalid-feedback">Please choose a Email.</div>
-    </div>
-
-    <input type="button" value="Submit" class="btn btn-secondary w-100 mt-2" @click="saveForm" />
-  </form>
+  </div>
 </template>
